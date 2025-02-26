5ab3039f9f1d19e06fe17b2f6a7250362e74add1
0xcodercrane <codercrane@gmail.com>
2025-02-26 23:53:13 +0800

    refactor: update Dashboard layout, and add TradingView chart

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,10 @@
 import { BitcoinPriceTracker } from "@/components/BitcoinPriceTracker";
-
+import TradingViewWidget from "@/components/chart";
 export default function Dashboard() {
   return (
-    <div className="py-10 flex justify-center items-center h-full">
+    <div className="py-10 flex flex-col sm:flex-row justify-center h-full">
       <BitcoinPriceTracker />
+      <TradingViewWidget type="BTCUSDT" />
     </div>
   );
 }
